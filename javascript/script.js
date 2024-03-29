d84cae17234052dd45437b0005ee7a6f2bb906d5
cauagarrido <cauagarrido@gmail.com>
2024-03-29 13:11:23 -0300

    alteração da foto

--- javascript/script.js
+++ javascript/script.js
@@ -58,3 +58,45 @@ $(document).ready(function() {
         distance: '20%'
     })
 });
+
+// function sleep(ms){
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// const phrases = ['Arquiteta', 'Designer de Interiores'];
+// const el = document.getElementById("typewriter");
+
+// let sleepTime = 100;
+
+// let curPhraseIndex = 0;
+
+// const writeLoop = async () => {
+//     while(true){
+//         let curWord = phrases[curPhraseIndex];
+        
+//         for(let i = 0; i < curWord.length; i++){
+//             el.innerText = curWord.substring(0, i + 1)
+//             await sleep(sleepTime)
+//         }
+
+//         await sleep(sleepTime * 10);
+
+//         for(let i = 0; i < curWord.length; i--){
+//             el.innerText = curWord.substring(0, i + 1)
+//             await sleep(sleepTime)
+//         }
+
+//         await sleep(sleepTime * 5);
+
+
+//         if(curPhraseIndex === phrases.length - 1){
+//             curPhraseIndex = 0;
+//         }else{
+//             curPhraseIndex++;
+//         }
+
+
+//     }
+// };
+
+// writeLoop();
